b49be90bf4fbe8b33d57793728799b0cf228d501
hackernogg <61939092+hackernogg@users.noreply.github.com>
2020-09-11 00:20:14 +0200

    Create hello.java

---
+++ Java/selfstudy/hello.java
@@ -0,0 +1,16 @@
+public class hello {
+
+ static public void main(String[] args) {
+	 /*public static void main(String[] args)
+
+public: So that JVM can execute the method from anywhere.
+static: Main method is to be called without object. 
+The modifiers public and static can be written in either order.
+void: The main method doesn't return anything.
+main(): Name configured in the JVM.
+String[]: The main method accepts a single argument: 
+          an array of elements of type String.
+		  */
+ System.out.println("Hello World");
+}
+}
