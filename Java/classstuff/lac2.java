fee08fc009d54e94ebbe373d02f8a967d80d0d02
hackernogg <61939092+hackernogg@users.noreply.github.com>
2020-09-17 11:18:53 +0200

    Create lac2.java

---
+++ Java/classstuff/lac2.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+public class lac2 {
+    
+    public static void main (String[] args){
+
+    public static void main (String[] args){
+        int x = 5 ;
+        int y = 10;
+        int z = ++x * y--;
+        System.out.println(z);
+    }
+}
+------------------------------------------------
+//if loop
+
+if(true){
+    //I run
+}
+Int try;
+if(try>=20)
+{System.out.println("I work");
+System.out.println("I work always");
+
+if(condition){
+    if{};
+    if{};
+}
+else if (){}
+else {}
+}
+---------------------------------------
+//Switch case
+
+Scanner keyboard = new Scanner(System.in);
+System.out.println("Gay type scanner: enter ur type!♂♀");
+int myint = keyboard.nextInt();
+
+switch (myint){
+    case 1: System.out.println("u are 1 u need 0");
+    break;//to stop not to match next one.
+                // break;
+   // case value2: prg2;
+     //            break;
+     case 0: System.out.println("u are 0 u need 1");
+     break;
+    default : System.out.println("meh!");  
+    break;
+}
+
+
+
+}
+-------------------------------------------
+//while loop
+int number=3;
+while(number>3){
+    System.out.println("u are retard");
+}
+
+}
+import java.util.Scanner;
+Scanner keyboard = new Scanner(System.in);
+System.out.println("Gay type scanner: enter ur type!♂♀");
+int myint = keyboard.nextInt();
+//get input from the keybord
