ed988b5faeab02bb0c0fa3482b148f596093865a
hackernogg <61939092+hackernogg@users.noreply.github.com>
2020-09-28 16:49:08 +0200

    Create hw1.java

---
+++ Java/HW/hw1.java
@@ -0,0 +1,12 @@
+public class hw1{
+    public static void main (String args[]){
+    int max = Integer.parseInt(args[0]);
+    for(int i=1;i < args.length;i++){
+        int nums = Integer.parseInt(args[i]);
+      if(nums > max){
+        max = nums;
+      }
+    }
+    System.out.println(max);
+  }
+}
