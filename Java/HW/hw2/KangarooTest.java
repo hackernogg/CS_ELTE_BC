8f2573b218cfec24537325b7f0e7dd5bccf7f376
hackernogg <61939092+hackernogg@users.noreply.github.com>
2020-10-06 17:58:56 +0200

    Create KangarooTest.java

---
+++ Java/HW/hw2/KangarooTest.java
@@ -0,0 +1,12 @@
+import roo.Kangaroo;
+
+public class KangarooTest {
+    public static void main(String[]args)
+    {
+        Kangaroo first = new Kangaroo("Felix", 100);
+        //Kangaroo second = new Kangaroo(3);
+        
+        first.display("Ausztralia");
+    }
+    
+}
