fc15b3040c1fc99319b4a26605feabaf15cb2d59
hackernogg <61939092+hackernogg@users.noreply.github.com>
2021-05-02 14:44:20 +0200

    Create function.h

---
+++ OPS/HW2/function.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+
+int add(int argc, char **argv);
+int change(int argc, char **argv);
+int dele(int argc, char **argv);
+int total(int argc, char **argv);
+int count(int argc, char **argv);
+int vac();
+
+#endif
