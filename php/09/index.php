76288cbe1ff629e9ba5619139401787c696b5a8b
Sun Yilong <61939092+hackernogg@users.noreply.github.com>
2021-11-30 20:40:49 +0100

    Create index.php

---
+++ php/09/index.php
@@ -0,0 +1,43 @@
+<?php 
+    // process 
+    // Task2/a
+    /*-------------------
+    $name = 'Jim raynor';
+    var_dump($_GET);
+    //https://webprogramozas.inf.elte.hu/hallgatok/bcfogs/9/?%20a=1&b=2&c[]=1&c[]=4
+    */
+    if( isset($_GET['name'])){
+        $name = $_GET['name'];
+    }
+    //task2/b
+    if( isset($_POST['nameFromForm'])){
+        $nameFromForm = $_POST['nameFromForm'];
+    }
+    print_r($_SERVER);
+    //render
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p><?="hello world"?></p>
+    <h1>task 2.</h1>
+    <h2>a)</h2>
+    <?php if (isset($name)) :?>
+    <p>hello <?= $name ?>!</p>
+    <?php endif; ?>
+    <h2>b)</h2>
+    <form method="post">
+        <input name="nameFromForm">
+        <input type="submit" value="Let's go">
+    </form>
+    <?php if (isset($nameFromForm)) :?>
+    <p>hello <?= $nameFromForm ?>!</p>
+    <?php endif; ?>
+</body>
+</html>
